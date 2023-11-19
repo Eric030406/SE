7908da9f1fdff4acd80ed9186e53cb4350909a36
Eric <real.dassy@gmail.com>
2023-11-19 23:51:32 +0900

    first commit

---
+++ SE/SE/SEApp.swift
@@ -0,0 +1,17 @@
+//
+//  SEApp.swift
+//  SE
+//
+//  Created by 송지혁 on 11/19/23.
+//
+
+import SwiftUI
+
+@main
+struct SEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
